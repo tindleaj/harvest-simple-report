73c30f8d8bff224a246e866f95d400fda35ea4a5
Austin Tindle <tindleaj@gmail.com>
2017-05-08 20:30:49 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const BASE = 'http://austintindle.harvestapp.com/';
+var projectID = '13871903';
+var start = '20170301';
+var end = '20170508';
+var url = '/projects/' + projectID + '/entries?from=' + start + '&to=' + end;
+
+axios.get(url, {
+	baseURL: BASE,
+	headers: {
+		"Content-Type": 'application/json',
+		"Accept": 'application/json',
+		"Authorization": 'Basic dGluZGxlYWpAZ21haWwuY29tOkZzazUtIXcvUDRNaypGPW4zQQ=='
+	} 
+}).then((res) => {
+	res.data.forEach((entry) => {
+		getTaskByID(entry.day_entry.task_id).then((res) => {
+			console.log(`[${res.task.name}] - ${entry.day_entry.notes}`);
+		})
+	});
+});
+
+function getTaskByID(id) {
+	return new Promise((resolve, reject) => {
+		axios.get('tasks/' + id, {
+			baseURL: BASE,
+			headers: {
+				"Content-Type": 'application/json',
+				"Accept": 'application/json',
+				"Authorization": 'Basic dGluZGxlYWpAZ21haWwuY29tOkZzazUtIXcvUDRNaypGPW4zQQ=='
+			}
+		}).then((res) => {
+			resolve(res.data);
+		});
+	});
+}
+
+function getTaskByName(name) {
+	// Get all tasks
+	getAllTasks().then((res) => {
+		res.forEach((entry) => {
+			// if task name is the parameter passed
+			if (entry.task.name === name) {
+				var id = entry.task.id;
+				getTaskByID(id).then((res) => {
+					console.log(res);
+				});
+			}
+		});
+	});	
+}
+
+function getAllTasks() {
+	return new Promise((resolve, reject) => {
+		axios.get('tasks/', {
+			baseURL: BASE,
+			headers: {
+				"Content-Type": 'application/json',
+				"Accept": 'application/json',
+				"Authorization": 'Basic dGluZGxlYWpAZ21haWwuY29tOkZzazUtIXcvUDRNaypGPW4zQQ=='
+			}
+		}).then((res) => {
+			resolve(res.data);
+		});
+	});
+}
