58e0d16395c266d3904d7d06a7c879189a7b9eca
Austin Tindle <tindleaj@gmail.com>
2017-05-16 11:40:13 -0400

    added printReport function

--- app.js
+++ app.js
@@ -2,26 +2,33 @@ const axios = require('axios');
 const fs = require('fs');
 
 const BASE = 'http://austintindle.harvestapp.com/';
-var projectID = '13871903';
-var start = '20170301';
-var end = '20170508';
-var url = '/projects/' + projectID + '/entries?from=' + start + '&to=' + end;
+var generalID = '13718240'; // Seal redesign: 13871903; General: 13718240;
+var sealID = '13871903'; // Seal redesign: 13871903; General: 13718240;
+var start = '20170505';
+var end = '20170518';
+var url1 = '/projects/' + generalID + '/entries?from=' + start + '&to=' + end;
+var url2 = '/projects/' + sealID + '/entries?from=' + start + '&to=' + end;
 
 // refactor this
-axios.get(url, {
-	baseURL: BASE,
-	headers: {
-		"Content-Type": 'application/json',
-		"Accept": 'application/json',
-		"Authorization": 'Basic dGluZGxlYWpAZ21haWwuY29tOkZzazUtIXcvUDRNaypGPW4zQQ=='
-	} 
-}).then((res) => {
-	res.data.forEach((entry) => {
-		getTaskByID(entry.day_entry.task_id).then((res) => {
-			console.log(`[${res.task.name}] - ${entry.day_entry.notes}`);
-		})
+function printReport(url) {
+	axios.get(url, {
+		baseURL: BASE,
+		headers: {
+			"Content-Type": 'application/json',
+			"Accept": 'application/json',
+			"Authorization": 'Basic dGluZGxlYWpAZ21haWwuY29tOkZzazUtIXcvUDRNaypGPW4zQQ=='
+		} 
+	}).then((res) => {
+		res.data.forEach((entry) => {
+			var spent_at = entry.day_entry.spent_at;
+			var hours = entry.day_entry.hours;
+
+			getTaskByID(entry.day_entry.task_id).then((res) => {
+				console.log(`${spent_at}: [hrs: ${hours}] [${res.task.name}] - ${entry.day_entry.notes}`);
+			})
+		});
 	});
-});
+}
 
 function getTaskByID(id) {
 	return new Promise((resolve, reject) => {
@@ -67,3 +74,5 @@ function getAllTasks() {
 		});
 	});
 }
+
+printReport(url1);
